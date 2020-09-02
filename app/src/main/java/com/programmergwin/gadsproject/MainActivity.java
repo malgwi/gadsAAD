7082794dcac9fad41dc5b1308cc91ea0b3b80268
GWINA <babsgodwin@gmail.com>
2020-09-02 20:53:33 +0100

    splash screen done

--- app/src/main/java/com/programmergwin/gadsproject/MainActivity.java
+++ app/src/main/java/com/programmergwin/gadsproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.programmergwin.gadsproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //omeooi
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                       //got to activity after delay for 3 secs
+                        startActivity(new Intent(MainActivity.this, LeaderBoardActivity.class));
+                    }
+                });
+            }
+        }, 3000);
+
     }
 }
